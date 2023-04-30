bd8029fa5e32ffc5fbbb1105466dd8852591c9e0
topmonitoring <dodoboy9620@gmail.com>
2023-05-01 00:37:33 +0300

    ci: updated lint config

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import HamburgerMenu from './hamburgerMenu.component';
 import MobileNav from './mobileNav.component';
-import { StyledHeder,StyledBlurSection } from './header.styles';
+import { StyledHeder, StyledBlurSection } from './header.styles';
 
 const Header: React.FC = () => {
   const [open, setOpen] = useState(false);
@@ -13,8 +13,12 @@ const Header: React.FC = () => {
       <StyledHeder>
         <HamburgerMenu open={open} toggle={handleNavbar} />
       </StyledHeder>
-      <MobileNav open={open}/>
-      <StyledBlurSection className={open ? `blur-2xl` : ``} style={open ? {zIndex:'5' ,cursor:'pointer'} : {zIndex:'0'}} onClick={handleNavbar}/>
+      <MobileNav open={open} />
+      <StyledBlurSection
+        className={open ? `blur-2xl` : ``}
+        style={open ? { zIndex: `5`, cursor: `pointer` } : { zIndex: `0` }}
+        onClick={handleNavbar}
+      />
     </>
   );
 };
--- src/sections/about/about.component.tsx
+++ src/sections/about/about.component.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import { StyledAboutSection } from './about.styles';
 import ReactMarkdown from 'react-markdown';
-import { useStaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby';
 
 const AboutSection: React.FC = () => {
-  const  data  = useStaticQuery(
+  const data = useStaticQuery(
     graphql`
-    query {
-      markdownRemark(frontmatter: {templateKey: {eq: "about-section"}}) {
-        frontmatter {
-          authorName
-          authorPick
-          description
+      query {
+        markdownRemark(frontmatter: { templateKey: { eq: "about-section" } }) {
+          frontmatter {
+            authorName
+            authorPick
+            description
+          }
         }
       }
-    }
-    `)
-  const { markdownRemark } = data 
-  const { frontmatter } = markdownRemark
+    `,
+  );
+  const { markdownRemark } = data;
+  const { frontmatter } = markdownRemark;
   return (
     <>
       <StyledAboutSection>
-        <div className="text-3xl font-bold underline ">{frontmatter.authorName}</div>
-        <img style={{width:"300px",height:'300px'}} src={frontmatter.authorPick}></img>
+        <div className="text-3xl font-bold underline ">
+          {frontmatter.authorName}
+        </div>
+        <img
+          style={{ width: `300px`, height: `300px` }}
+          src={frontmatter.authorPick}
+        ></img>
         <ReactMarkdown>{frontmatter.description}</ReactMarkdown>
       </StyledAboutSection>
     </>
